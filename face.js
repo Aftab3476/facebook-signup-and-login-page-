733f9437176f2844015a7a77a230511e6dacdb5f
Aftab3476 <aftabalam8521068625@gmail.com>
2022-09-17 14:16:25 +0530

    facebook  login and signup page

---
+++ face.js
@@ -0,0 +1,53 @@
+// function singup() {
+//     var fstname = document.getElementById("fname").value;
+//     var srname = document.getElementById("sname").value;
+//     var adrs = document.getElementById("address").value;
+//     var pswad = document.getElementById("password").value;
+//     var daate = document.getElementById("date").value;
+//     var months = document.getElementById("month").value;
+//     var years = document.getElementById("year").value;
+//     var fmale = document.getElementById("female").value;
+//     var maale = document.getElementById("male").value;
+//     var cstm = document.getElementById("custom").value;
+
+
+//     localStorage.setItem("frstname", fstname)
+//     localStorage.setItem("srname1", srname)
+//     localStorage.setItem("adrs1", adrs)
+//     localStorage.setItem("pswad1", pswad)
+//     localStorage.setItem("daate1", daate)
+//     localStorage.setItem("months1", months)
+//     localStorage.setItem("years1", years)
+//     localStorage.setItem("fmale1", fmale)
+//     localStorage.setItem("maale1", maale)
+//     localStorage.setItem("cstm1", cstm)
+// }
+
+const jediArr = [
+    {
+        id: 1,
+        name:"Aftab"
+    },
+    {
+        id: 2,
+        name:"mahtab"
+    },
+    {
+        id: 3,
+        name:"shamshad"
+    },
+    {
+        id: 4,
+        name:"amir"
+    },
+    {
+        id: 5,
+        name:"habibullah"
+    },
+    {
+        id: 6,
+        name:"arman"
+    },
+
+]
+console.log(jediArr)
